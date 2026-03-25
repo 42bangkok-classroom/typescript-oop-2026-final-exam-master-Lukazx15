8297ab737b637e2b52a986f3f5cd71c362a190e2
event <event@z3t1c3.42bangkok.com>
2026-03-25 11:35:48 +0700

    p00.03

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as responseInterface from './interfaces/response.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): responseInterface.ApiResponse<{
+    service: string;
+    version: string;
+  }> {
+    return {
+      success: true,
+      data: this.appService.getHello(),
+      message: 'Hello NestJS',
+    };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,16 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    throw new Error('Method not implemented.');
-  }
-  private readonly Data = {
-    success: true,
-    data: {
+  getHello(): {
+    service: string;
+    version: string;
+  } {
+    return {
       service: 'purchase-api',
       version: '1.0.0',
-    },
-    message: 'Hello NestJS',
-  };
-  return: any;
+    };
+  }
 }
