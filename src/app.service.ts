ae800f4980de271f3e3eedb45d832a914c286078
event <event@z3t1c3.42bangkok.com>
2026-03-25 11:17:27 +0700

    p00.02

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
-
-
-
 @Injectable()
 export class AppService {
   getHello(): string {
     throw new Error('Method not implemented.');
   }
   private readonly Data = {
-    "success": true,
-  "data": {
-    "service": "purchase-api",
-    "version": "1.0.0"
-  },
-  "message": "Hello NestJS"
-  }
-    return: any  ;
-  }
-
+    success: true,
+    data: {
+      service: 'purchase-api',
+      version: '1.0.0',
+    },
+    message: 'Hello NestJS',
+  };
+  return: any;
+}
